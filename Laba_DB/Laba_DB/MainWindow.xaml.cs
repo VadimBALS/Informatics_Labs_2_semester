35ad1a3deee88ef2efaf29e142041bbaa3a3b06f
Vadim_L <svobodolubsev@inbox.ru>
2023-02-04 17:29:42 +0700

    Сделал лабу по базе данных

--- Laba_DB/Laba_DB/MainWindow.xaml.cs
+++ Laba_DB/Laba_DB/MainWindow.xaml.cs
@@ -20,12 +20,12 @@ using Microsoft.Win32;
 /*
 Разработать программное приложение WPF, имеющее графический интерфейс и реализующее
 следующие функции:
-    1) Ввод данных о студентах: уникальный номер, фио, оценка по физике, оценка по    
+    1) Ввод данных о студентах: уникальный номер, фио, оценка по физике, оценка по    YES
 математике.
-    2) Добавление данных в таблицу базы данных SQLite через интерфейс приложения.     
+    2) Добавление данных в таблицу базы данных SQLite через интерфейс приложения.     YES
     3) Чтение данных из базы данных SQLite и отображение их на форме приложения.      YES
-    4) Редактирование данных в базе данных SQLite через интерфейс приложения.         
-    5) Удаление данных из таблиц                                                      
+    4) Редактирование данных в базе данных SQLite через интерфейс приложения.         YES
+    5) Удаление данных из таблиц                                                      YES
 
 База данных должна содержать 2 таблицы, связанные через уникальный номер:
     1. Таблица, содержащая уникальный номер и фио.                                    YES
@@ -54,7 +54,7 @@ namespace Laba_DB
 {
     public partial class MainWindow : Window
     {
-        // подключение к БД
+        // переменная для подключения к БД
         SQLiteConnection m_dbConnection;
 
         public MainWindow()
@@ -62,7 +62,10 @@ namespace Laba_DB
             InitializeComponent();
 
             /*
-            SQLiteConnection.CreateFile("C:\\Users\\user\\Desktop\\HEI\\Semestr_2\\Informatics\\C#\\Laba_DB\\test.db");
+            
+            // ################### CREATE DB FILE ################### 
+             
+            SQLiteConnection.CreateFile(""D:\\MyDocs\\method\\intsys\\test.db"");
 
             // открытие окна для поиска файла БД
             OpenFileDialog dlg = new OpenFileDialog();
@@ -77,6 +80,7 @@ namespace Laba_DB
             //открытие соединения с базой данных
             m_dbConnection.Open();
 
+            // ################### CREATE 2 TABLES ################### 
 
             string sql = "CREATE TABLE students (uid INTEGER NOT NULL, fio TEXT NOT NULL, PRIMARY KEY (\"uid\"))";
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
@@ -86,6 +90,8 @@ namespace Laba_DB
             command = new SQLiteCommand(sql, m_dbConnection);
             command.ExecuteNonQuery();
 
+
+            // ################### INSERT 3 STUDENTS ################### 
 
             sql = $"INSERT INTO `grades` (`uid`, `math`, `phys`) VALUES (1, 3, 3);" +
                   $"INSERT INTO `students` (`uid`, `fio`) VALUES (1, \"Student 1\");";
@@ -104,13 +110,33 @@ namespace Laba_DB
             */
         }
 
-        // структура для записи данных в БД
+        // структура для записи данных из двух таблиц
         struct SField
         {
             public string fio { get; set; }
             public int math { get; set; }
             public int phys { get; set; }
+            public int uid { get; set; }
         }
+
+        // структура для записи данных из таблицы stydents
+        struct STuds
+        {
+            public string fio { get; set; }
+            public int uid { get; set; }
+        }
+
+        // структура для записи данных из таблицы grades
+        struct GRades
+        {
+            public int math { get; set; }
+            public int phys { get; set; }
+            public int uid { get; set; }
+        }
+
+
+        // отслеживание сколько всего студентов в БД
+        int number_of_students = 0;
 
         private void Button_Click_start_db_con(object sender, RoutedEventArgs e)
         {
@@ -130,8 +156,14 @@ namespace Laba_DB
         
         private void Button_Click_show_data(object sender, RoutedEventArgs e)
         {
+            // очистка БД
+            this.data.Items.Clear();
+
+            // обнуление кол-вa студентов 
+            number_of_students = 0;
+
             // формирование запроса для вывода данных из БД (ФИО, оценка матеша, оценка физика)
-            string sql = "select students.fio, grades.math, grades.phys from students, grades where students.uid=grades.uid";
+            string sql = "select students.uid, students.fio, grades.math, grades.phys from students, grades where students.uid=grades.uid";
             
             // задаётся команда 
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
@@ -150,10 +182,47 @@ namespace Laba_DB
                 f.fio = reader["fio"].ToString();
                 f.math = int.Parse(reader["math"].ToString());
                 f.phys = int.Parse(reader["phys"].ToString());
+                f.uid = int.Parse(reader["uid"].ToString());
 
                 // запись данных в DataGrid
                 data.Items.Add(f);
+
+                number_of_students++;
             }
+
+            // работа с табоицей students
+            this.data_stud.Items.Clear();
+            sql = "select students.uid, students.fio from students";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                STuds f = new STuds();
+
+                f.uid = int.Parse(reader["uid"].ToString());
+                f.fio = reader["fio"].ToString();
+                
+                data_stud.Items.Add(f);
+            }
+
+            // работа с таблицей grades
+            this.data_grad.Items.Clear();
+            sql = "select grades.uid, grades.math, grades.phys from grades";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                GRades f = new GRades();
+
+                f.uid = int.Parse(reader["uid"].ToString());
+                f.phys = int.Parse(reader["phys"].ToString());
+                f.math = int.Parse(reader["math"].ToString());
+
+                data_grad.Items.Add(f);
+            }
+
+            // вывод кол-ва студентов 
+            lb.Content = "Number of students: " + number_of_students;
         }
 
         private void Button_Click_add_student(object sender, RoutedEventArgs e)
@@ -162,44 +231,67 @@ namespace Laba_DB
 
             if (window.ShowDialog() == true)
             {
-                int count_students = 0;
-                
-                string sql = "SELECT* FROM students, grades WHERE students.uid = grades.uid";
-                
-                // задаётся команда 
+                // добавление новенького
+                number_of_students++;
+
+                // формирование запроса
+                string sql = $"INSERT INTO `grades` (`uid`, `math`, `phys`) VALUES ( {number_of_students}, {int.Parse(window.g_math.Text)}, {int.Parse(window.g_phys.Text)});" +
+                             $"INSERT INTO `students` (`uid`, `fio`) VALUES ( {number_of_students}, \"{window.name.Text}\");";
+
+                // отправка запроса  
                 SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-
-                // извлекается 
-                SQLiteDataReader reader = command.ExecuteReader();
-                
-                // подсчёт скока всего студентов в таблице
-                while (reader.Read())
-                    count_students++;
-
-                // кол-во студентов 
-                lb.Content = count_students++;
-
-                // формирование запроса для вывода данных из БД (ФИО, оценка матеша, оценка физика)
-                sql = $"INSERT INTO `grades` (`uid`, `math`, `phys`) VALUES ( {count_students}, {int.Parse(window.g_math.Text)}, {int.Parse(window.g_phys.Text)});" +
-                      $"INSERT INTO `students` (`uid`, `fio`) VALUES ( {count_students}, \"{window.name.Text}\");";
-
-                // задаётся команда 
-                command = new SQLiteCommand(sql, m_dbConnection);
                 command.ExecuteNonQuery();
-
-                // кол-во студентов 
-                lb.Content = count_students+1;
             }
         }
 
         private void Button_Click_delete_student(object sender, RoutedEventArgs e)
         {
+            // проверка на наличие выделенного поля в гриде 
+            if (data.SelectedIndex > -1)
+            {
+                // создание экземпляра структуры и загрузка в неё данных из грида
+                SField test = (SField)(data.SelectedItem);
 
+                // создание запроса 
+                string sql = $"DELETE FROM students WHERE uid = {test.uid};" +
+                             $"DELETE FROM grades WHERE uid = {test.uid};";
+
+                // отправка запроса 
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteNonQuery();
+
+                number_of_students--;
+            }
+        }
+
+
+        // ################### Как передать инфу в Wnd_2 ? ################### 
+        private void edit_entry_Click(object sender, RoutedEventArgs e)
+        {
+            // проверка на наличие выделенного поля в гриде 
+            if (data.SelectedIndex > -1)
+            {
+                // создание экземпляра структуры и загрузка в неё данных из грида
+                SField test = (SField)(data.SelectedItem);
+
+                Wnd_2 window = new Wnd_2();
+
+                if (window.ShowDialog() == true)
+                {
+                    // создание запроса 
+                    string sql = $"UPDATE students SET fio = \"{window.name.Text}\" WHERE uid = {test.uid};" +
+                                 $"UPDATE grades SET math = {int.Parse(window.g_math.Text)}, phys = {int.Parse(window.g_phys.Text)} WHERE uid = {test.uid};";
+
+                    // отправка запроса 
+                    SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
-            //закрытие соединения с базой данных
+            //закрытие соединения с базой данных перед закрытием прилодения
             if (m_dbConnection != null)
                 m_dbConnection.Close();
         }
